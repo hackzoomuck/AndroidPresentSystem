6a84a428301266c521d4a0b7d8570e9409ca77c3
crystal9619 <sj5173@naver.com>
2018-05-09 03:13:14 +0900

    수정 0509 인생 힘들다..

--- app/src/main/java/com/example/leeje/androidpresentsystem/ListOnline.java
+++ app/src/main/java/com/example/leeje/androidpresentsystem/ListOnline.java
@@ -1,5 +1,6 @@
 package com.example.leeje.androidpresentsystem;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
@@ -9,19 +10,20 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 
 public class ListOnline extends AppCompatActivity {
 
-    //? e되는건가?
-    //ㅇㄹㅇㄹㅇㅇㄹㅇ....
-    //ㅋㅋ된다 된다~!!!!!!????????ㅇㄴㄻㄴㅇㅁㅇㄹㅇㄹㅇㄹㅇㄹㅇㄹㅇ
+    Button detail;
+    Button show;
+
     //firebase
     DatabaseReference onlineRef,currentUserRef, counterRef;
     FirebaseRecyclerAdapter<User,ListOnlineViewHolder> adapter;
-//쟂ㅇㅇㅇㅇ
     //View
 
     RecyclerView listOnline;
@@ -38,9 +40,28 @@ public class ListOnline extends AppCompatActivity {
         layoutManager=new LinearLayoutManager(this);
         listOnline.setLayoutManager(layoutManager);
 
+        //임시공간
+        detail = (Button) findViewById(R.id.promiseDetail);
+        detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MakeDetail.class);
+                startActivity(intent);
+            }
+        });
+
+        show = (Button) findViewById(R.id.mainMap);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
         //set toolbar and logout/join menu
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolBar);
-        toolbar.setTitle("EDMT Presnece System");
+        toolbar.setTitle("Promise Maker");
         setSupportActionBar(toolbar);
     }
 
---
+++ app/src/main/java/com/example/leeje/androidpresentsystem/MakeDetail.java
@@ -0,0 +1,13 @@
+package com.example.leeje.androidpresentsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_departure_layout);
+    }
+}
